a7f9c222c74280d9f73733b09727ce18850dcb67
Isabela Rodrigues <66086398+IsabelaSRodrigues@users.noreply.github.com>
2022-09-25 10:12:22 -0300

    Update Program.cs

--- AcademiaDotNetAtos/2 - Exercícios/Program.cs
+++ AcademiaDotNetAtos/2 - Exercícios/Program.cs
@@ -191,6 +191,44 @@ namespace AcademiaDotNetAtos
          
            
             */
+            
+            /*
+            
+            8) Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
+            A fórmula da conversão é F=(9*C+160)/5.
+            */
+            Console.Write("Digite a temperatura em graus Celsius:");
+            double tempC = Double.Parse(Console.ReadLine());
+
+            double tempF = (9 * tempC + 160) / 5;
+
+            Console.WriteLine("A temperatura em graus Fahrenheit é:" +tempF+" graus");
+            
+            /*
+            
+            9) Uma loja vende seus produtos no sistema de entrada mais duas prestações, sendo a entrada maior 
+            do que ou igual às prestações, que devem ser iguais, inteiras e as maiores possíveis. 
+            Por exemplo, se o valor da mercadoria for R$ 270,00, a entrada e as duas prestações são iguais a R$ 90,00; 
+            se o valor da mercadoria for R$ 302,75 a entrada é de R$ 102,75 e as duas prestações são iguais a R$ 100,00.
+            Escreva um programa em C# e no Visual Studio que receba o valor da mercadoria e forneça o valor 
+            da entrada e das duas prestações, de acordo com as regras acima. 
+            Observe que uma justificativa para a adoção desta regra é que ela facilita a confecção e o 
+            conseqüente pagamento dos boletos das duas prestações.
+            
+           */
+            Console.Write("Digite o valor do produto: R$");
+            double product = Double.Parse(Console.ReadLine());
+
+            double entrada = product / 2;
+            double parcela1 = entrada / 2;
+            double parcela2 = parcela1;
+
+
+            Console.WriteLine("A entrada será de: R$"+entrada);
+
+            Console.WriteLine("A primeira parcela será de: R$" + parcela1);
+            Console.WriteLine("A segunda parcela será de: R$" + parcela2);
+
         }
     }
 }
