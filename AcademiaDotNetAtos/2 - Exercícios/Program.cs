2d7ab28c74487cd6beab01da6d3a95aabd13370a
Isabela Rodrigues <66086398+IsabelaSRodrigues@users.noreply.github.com>
2022-10-01 13:24:36 -0300

    Update Program.cs

--- AcademiaDotNetAtos/2 - Exercícios/Program.cs
+++ AcademiaDotNetAtos/2 - Exercícios/Program.cs
@@ -238,7 +238,7 @@ namespace AcademiaDotNetAtos
             Console.WriteLine("A segunda parcela será de: R$" + parcela);
             
             /*
-            
+            /*
             10) Um programa para gerenciar os saques de um caixa eletrônico deve possuir algum mecanismo 
             para decidir o número de notas de cada valor que deve ser disponibilizado para o cliente que 
             realizou o saque. Um possível critério seria o da “distribuição ótima” no sentido de que as 
@@ -247,11 +247,39 @@ namespace AcademiaDotNetAtos
             uma nota de R$ 10,00, uma nota de R$ 5,00 e duas notas de R$ 1,00. 
             Escreva um programa que receba o valor da quantia solicitada e retorne a distribuição das notas 
             de acordo com o critério da “distribuição ótima”.
-
+            */
+            
+            /*
             11) Escreva um algoritmo para ler o número de eleitores de um município, 
             o número de votos brancos, nulos e válidos. 
             Calcule e escreva o percentual que cada um  representa em relação ao total de eleitores.
+            */
+            
+            int voters = 10000;
+            int valid = 6500;
+            int voteB = 1200;
+            int nulos = 2000;
 
+            double  percentV, percentVB, percentN;
+
+            percentV = (valid * 100) / voters;
+            percentVB = (voteB * 100) / voters;
+            percentN = (nulos * 100) / voters;
+
+            Console.Write("Total de eleitores: " +voters+ ",votos válidos: "+valid+ ",votos em branco: "+voteB+ " e votos nulos: "+nulos+".");
+            Console.Write("\r\n");
+
+            Console.Write("O valor percentual de votos válidos é: " + percentV + "%");
+            Console.Write("\r\n");
+
+            Console.Write("O valor percentual de votos em branco é: " + percentVB + "%");
+            Console.Write("\r\n");
+            
+            Console.Write("O valor percentual de votos nulos é: " + percentN + "%");
+            
+
+            
+            /*
             12) Um motorista de taxi deseja calcular o rendimento de seu carro na praça. Sabendo-se que o preço do combustível é de R$6,90, escreva um algoritmo para ler a 
             marcação do odômetro (marcador de quilometragem) no início do dia, a marcação no final do dia, o número de litros de combustível gasto e o valor total (R$) recebido 
             dos passageiros. Calcule e escreva a média do consumo em Km/l e o lucro líquido do dia.
